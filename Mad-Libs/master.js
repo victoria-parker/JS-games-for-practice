6c7a3121103fc9089df76270fdbc1bec90ed2e0c
victoria-parker <victoriaparkerb@gmail.com>
2021-08-27 17:07:38 +0100

    Mad Libs corrected

--- Mad-Libs/master.js
+++ Mad-Libs/master.js
@@ -6,30 +6,30 @@ if(confirmar){
     /* Player 1 */
         /* Characters */
         var name1p1=prompt('Player 1: Tell me the name of a person you know or a famous one')
-        var name2p1=prompt('Player 1: Tell me the name of a person you know or a famous one')
+        var name2p1=prompt('Player 1: Tell me the name of another person you know or a famous one')
 
 
         /* Location */
-        var wherep1=prompt('Player 1: Tell me a location')
+        var wherep1=prompt('Player 1: Tell me a location where the characters meet')
 
         /* Action */
-        var what1=prompt('Player 1: What did they do?')
+        var what1=prompt('Player 1: What did the characters do?')
 
         /* Conversation */
         var say1p1=prompt('Player 1: What did the first one say?')
         var say2p1=prompt('Player 1: What did the second one say?')
 
         /* Ending */
-        var endingp1=prompt('Player 1: Tell me an ending for this to characters')
+        var endingp1=prompt('Player 1: Tell me an ending for this characters')
 
     /* Player 2 */
 
         /* Characters */
         var name1p2=prompt('Player 2: Tell me the name of a person you know or a famous one')
-        var name2p2=prompt('Player 2: Tell me the name of a person you know or a famous one')
+        var name2p2=prompt('Player 2: Tell me the name of another person you know or a famous one')
 
         /* Location */
-        var wherep2=prompt('Player 2: Tell me a location')
+        var wherep2=prompt('Player 2: Tell me a location where the characters meet')
 
         /* Action */
         var what2=prompt('Player 2: What did they do?')
@@ -39,12 +39,12 @@ if(confirmar){
         var say2p2=prompt('Player 2: What did the second one say?')
 
         /* Ending */
-        var endingp2=prompt('Player 2: Tell me an ending for this to characters')
+        var endingp2=prompt('Player 2: Tell me an ending for this characters')
 
     /* Stories */
-    document.getElementById('player1').innerHTML+=('<p>'+name1p1+' '+ name2p2+' met each other at '+wherep1+' and they '+what2+' together. '+name1p1+' said: '+say1p1 +' and '+name2p2+' answered '+say2p2+'. And they '+endingp1+'.</p>')
+    document.getElementById('player1').innerHTML+=('<p>'+name1p1+' and '+ name2p2+' met each other in '+wherep1+' and they '+what2+' together. '+name1p1+' said: '+say1p1 +' and '+name2p2+' answered '+say2p2+'. And  '+endingp1+'.</p>')
 
-    document.getElementById('player2').innerHTML+=('<p>'+name1p2+' '+ name2p1+' met each other at '+wherep2+' and they '+what1+' together. '+name1p2+' said: '+say1p2 +' and '+name2p1+' answered '+say1p1+'. And they '+endingp2+'.</p>')
+    document.getElementById('player2').innerHTML+=('<p>'+name1p2+' and '+ name2p1+' met each other in '+wherep2+' and they '+what1+' together. '+name1p2+' said: '+say1p2 +' and '+name2p1+' answered '+say1p1+'. And  '+endingp2+'.</p>')
 }else{
     document.getElementById('stories').style.display='none';
     document.write('<h2>We\'ll play another day!</h2>')
