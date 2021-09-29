9f52fd78b67eb8c37d4a0999cd3eb5c360047c1a
victoria-parker <victoriaparkerb@gmail.com>
2021-09-29 14:41:49 +0100

    html and style built, js started

---
+++ whack-a-mole/app.js
@@ -0,0 +1,5 @@
+const square=document.querySelector('.square')
+const mole=document.querySelector('.mole')
+const timeLeft=document.querySelector('#time-left')
+
+let score=document.querySelector('#score')
